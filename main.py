afb0f66b93383fd844c577ed16d538f3623fee24
qvbcpe6 <120000006+qvbTor@users.noreply.github.com>
2024-05-12 12:09:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import numpy as np
 # Load the pre-trained model
 @st.cache(allow_output_mutation=True)
 def load_model():
-    model = tf.keras.models.load_model('FV_1.h5')
+    model = tf.keras.models.load_model('xxy.h5')
     return model
 
 # Function to preprocess the image
